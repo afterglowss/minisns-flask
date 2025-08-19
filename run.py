6de610033a2119372234887ab1a6ba315e0da3dd
afterglowss <shw2912@gmail.com>
2025-08-19 14:27:56 +0900

    refoctor: add host and port

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from app import create_app
 app = create_app()
 print(app.url_map)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 # This file is the entry point for running the Flask application.
 # It imports the create_app function from the app package and runs the application with debug mode enabled.
 # This allows for easier development and debugging of the Flask application.
